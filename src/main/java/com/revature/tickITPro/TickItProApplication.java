3f3a18f13d5452a39f2c4c54f25d0d9769d82a06
calebabeck2020 <calebabeck@gmail.com>
2022-08-30 16:50:27 -0400

    added additional configuration

--- src/main/java/com/revature/tickITPro/TickItProApplication.java
+++ src/main/java/com/revature/tickITPro/TickItProApplication.java
@@ -2,8 +2,10 @@ package com.revature.tickITPro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class TickItProApplication {
 
 	public static void main(String[] args) {
