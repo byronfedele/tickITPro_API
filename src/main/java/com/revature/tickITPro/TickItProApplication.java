2bfaf744cfc6c5a5dca5d8dae6193df44c58185d
calebabeck2020 <calebabeck@gmail.com>
2022-08-30 15:37:18 -0400

    Project Initialized with Spring Boot, first commit to tickITPro

---
+++ src/main/java/com/revature/tickITPro/TickItProApplication.java
@@ -0,0 +1,13 @@
+package com.revature.tickITPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickItProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickItProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/tickITPro/TickItProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.tickITPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickItProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
