6ec17b2d5c4ad1512958e0c928ae681c4f271561
koneissiaka023 <koneissiaka023@gmail.com>
2022-08-31 17:16:51 -0400

    Created ticket model and ticket request dto

--- src/main/java/com/revature/tickITPro/ticket/Ticket.java
+++ src/main/java/com/revature/tickITPro/ticket/Ticket.java
@@ -1,4 +1,68 @@
 package com.revature.tickITPro.ticket;
 
+import com.revature.tickITPro.subject.Subject;
+import com.revature.tickITPro.ticket.dto.Requests.NewTicketRequest;
+import com.revature.tickITPro.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ticket")
 public class Ticket {
+    @Id
+    @Column(name = "ticket_id")
+    private String id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "priority")
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "submission_date")
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn(name = "req_user_id", nullable = false)
+    private User userId;
+
+    @ManyToOne
+    @JoinColumn(name = "pro_user_id")
+    private User proUser;
+
+    @ManyToOne
+    @JoinColumn(name = "subject_id")
+    private Subject subjectId;
+
+    public Ticket(NewTicketRequest newTicketRequest){
+        this.id = newTicketRequest.getId();
+        this.description = newTicketRequest.getDescription();
+        this.priority = Priority.valueOf(newTicketRequest.getPriority().toUpperCase());
+        this.status = Status.PENDING;
+        this.date = newTicketRequest.getDate();
+    }
+
+
+
+    public enum Priority{
+        DEFAULT, LOW_PRIORITY,HIGH_PRIORITY
+    }
+
+    public enum Status{
+        PENDING, CONFIRMED, IN_PROGRESS, RESOLVED
+    }
+
+
 }
