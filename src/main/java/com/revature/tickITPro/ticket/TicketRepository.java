41972cac752ba201ac445bc8d2e00dc04dfcd319
koneissiaka023 <koneissiaka023@gmail.com>
2022-08-31 18:03:52 -0400

    Completed edit, response and repository for ticket

--- src/main/java/com/revature/tickITPro/ticket/TicketRepository.java
+++ src/main/java/com/revature/tickITPro/ticket/TicketRepository.java
@@ -1,4 +1,16 @@
 package com.revature.tickITPro.ticket;
 
-public interface TicketRepository {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TicketRepository extends CrudRepository<Ticket, String> {
+    @Query(value = "FROM Ticket where ticketId= :ticketId")
+    Optional<Ticket> findByTicketId(String ticketId);
+
+    @Query(value = "FROM Ticket where userId= :userId")
+    Optional<Ticket> findByUserId(String userId);
 }
