8271a9a312b7aff7cddca9bf2611af07c9d0f68f
calebabeck2020 <calebabeck@gmail.com>
2022-08-31 16:26:05 -0400

    added LoginCreds dto

---
+++ src/main/java/com/revature/tickITPro/util/web/dto/LoginCreds.java
@@ -0,0 +1,22 @@
+package com.revature.tickITPro.util.web.dto;
+
+public class LoginCreds {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
