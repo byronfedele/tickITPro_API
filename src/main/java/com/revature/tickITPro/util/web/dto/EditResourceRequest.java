b93aa4d84eea24e8ca22d2c7dc8bd0c2f9067063
calebabeck2020 <calebabeck@gmail.com>
2022-08-31 16:21:07 -0400

    added EditResourceRequest DTO, started work on Department package

---
+++ src/main/java/com/revature/tickITPro/util/web/dto/EditResourceRequest.java
@@ -0,0 +1,14 @@
+package com.revature.tickITPro.util.web.dto;
+
+public abstract class EditResourceRequest {
+
+    protected String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
