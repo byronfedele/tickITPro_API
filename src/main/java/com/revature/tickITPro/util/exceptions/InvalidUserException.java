f12bd41d00f436f7a926524b315910c476a501f3
calebabeck2020 <calebabeck@gmail.com>
2022-09-01 15:57:18 -0400

    merge party

--- src/main/java/com/revature/tickITPro/util/exceptions/InvalidUserException.java
+++
@@ -1,7 +0,0 @@
-package com.revature.tickITPro.util.exceptions;
-
-public class InvalidUserException extends RuntimeException {
-    public InvalidUserException(String s) {
-        super(s);
-    }
-}
--- src/main/java/com/revature/tickITPro/util/exceptions/UnauthorizedException.java
+++ src/main/java/com/revature/tickITPro/util/exceptions/UnauthorizedException.java
@@ -1,7 +1,6 @@
 package com.revature.tickITPro.util.exceptions;
 
 public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException() {}
     public UnauthorizedException(String s) {
         super(s);
     }
