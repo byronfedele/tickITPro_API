10cda4e158a8c067e5650e062e148ea5afe3d1be
calebabeck2020 <calebabeck@gmail.com>
2022-09-01 15:45:02 -0400

    added no args constructor to UnauthorizedException

--- src/main/java/com/revature/tickITPro/util/exceptions/UnauthorizedException.java
+++ src/main/java/com/revature/tickITPro/util/exceptions/UnauthorizedException.java
@@ -1,6 +1,7 @@
 package com.revature.tickITPro.util.exceptions;
 
 public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException() {}
     public UnauthorizedException(String s) {
         super(s);
     }
