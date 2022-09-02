e13d1293442f09fca6f61132e19126c64b44b0dd
calebabeck2020 <calebabeck@gmail.com>
2022-09-02 12:11:36 -0400

    finished TokenGenerator for JWT Integration

--- src/main/java/com/revature/tickITPro/util/web/auth/TokenGenerator.java
+++ src/main/java/com/revature/tickITPro/util/web/auth/TokenGenerator.java
@@ -1,6 +1,11 @@
 package com.revature.tickITPro.util.web.auth;
 
+import com.revature.tickITPro.util.web.auth.dto.response.Principal;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class TokenGenerator {
@@ -11,5 +16,16 @@ public class TokenGenerator {
         this.jwtConfig = jwtConfig;
     }
 
-    public String createToken(Princi)
+    public String createToken(Principal principalUser) {
+        long now = System.currentTimeMillis();
+        JwtBuilder tokenBuilder = Jwts.builder()
+                .setId(principalUser.getId())
+                .setSubject(principalUser.getEmail())
+                .setIssuer("tickITPro")
+                .claim("isAdmin", principalUser.isAdmin())
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + jwtConfig.getExpiration()))
+                .signWith(jwtConfig.getSigAlg(),jwtConfig.getSigningKey());
+        return tokenBuilder.compact();
+    }
 }
