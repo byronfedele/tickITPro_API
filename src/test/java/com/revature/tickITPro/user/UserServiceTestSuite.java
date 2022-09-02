296a958da9a75765147fe4a15746a88b6c6ed6e3
calebabeck2020 <calebabeck@gmail.com>
2022-09-02 00:02:25 -0400

    started UserTestSuite, researching other testing options with Springboot

--- src/test/java/com/revature/tickITPro/user/UserServiceTestSuite.java
+++ src/test/java/com/revature/tickITPro/user/UserServiceTestSuite.java
@@ -1,4 +1,26 @@
 package com.revature.tickITPro.user;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceTestSuite {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    public boolean test_isUserValid_returnTrue_givenValidUser() {
+
+    }
+
+    @Test
+    public boolean test_isUserValid_returnTrue_givenValidUser() {
+
+    }
 }
