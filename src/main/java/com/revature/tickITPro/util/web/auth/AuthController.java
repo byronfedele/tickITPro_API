425509449cfbe97baf159555e806c85c9dd60ab0
calebabeck2020 <calebabeck@gmail.com>
2022-09-02 13:05:09 -0400

    Finished changes to AuthController for JWT Integration

--- src/main/java/com/revature/tickITPro/util/web/auth/AuthController.java
+++ src/main/java/com/revature/tickITPro/util/web/auth/AuthController.java
@@ -4,10 +4,12 @@ package com.revature.tickITPro.util.web.auth;
 import com.revature.tickITPro.user.User;
 import com.revature.tickITPro.user.UserService;
 import com.revature.tickITPro.util.web.auth.dto.request.LoginCreds;
+import com.revature.tickITPro.util.web.auth.dto.response.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @CrossOrigin(exposedHeaders = "Authorization")
@@ -16,18 +18,23 @@ import javax.servlet.http.HttpSession;
 public class AuthController {
 
     private final UserService userService;
+    private final TokenService tokenService;
 
     // Dependency Injection
     @Autowired
-    public AuthController(UserService userService){
+    public AuthController(UserService userService, TokenService tokenService){
         this.userService = userService;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void authorizeUser(LoginCreds loginCreds, HttpSession httpSession){
+    public Principal authorizeUser(@RequestBody LoginCreds loginCreds, HttpServletResponse resp){
         User authUser = userService.login(loginCreds.getEmail(),  loginCreds.getPassword());
-        httpSession.setAttribute("authUser", authUser);
+        Principal payload = new Principal(authUser);
+        String token = tokenService.generateToken(payload);
+        resp.setHeader("Authorization",token);
+        return payload;
     }
 
     @DeleteMapping
